ad5f85b54c1f28710308e1a608b89f1e7fc30cee
AnthonWellsjo <anthonwellsjo@gmail.com>
2021-07-25 08:52:23 +0200

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+  <svg version="1.1" width="50" height="32" viewBox="0 0 25 16" class="octicon octicon-logo-gist" aria-hidden="true">
+    <use xlink:href="#logo-gist" />
+  </svg>
+</footer>
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
