1f5f3e6d93a40b4cb0ea7998785f522bba3be211
AnthonWellsjo <anthonwellsjo@gmail.com>
2021-07-25 09:30:54 +0200

    home page and style

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,29 +1,30 @@
 <script lang="ts">
 	import Header from '$lib/header/Header.svelte';
-	import '../app.css';
+	import '../app.scss';
 </script>
 
 <Header />
 
 <main>
-	<slot />
+	<div id="frame">
+		<slot />
+	</div>
 </main>
 
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-  <svg version="1.1" width="50" height="32" viewBox="0 0 25 16" class="octicon octicon-logo-gist" aria-hidden="true">
-    <use xlink:href="#logo-gist" />
-  </svg>
-</footer>
+<footer />
 
 <style>
+  #frame{
+    width:99%;
+		max-width: 1024px;
+  }
 	main {
 		flex: 1;
+    width: 100%;
 		display: flex;
 		flex-direction: column;
 		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
+		align-items: center;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
@@ -34,10 +35,6 @@
 		justify-content: center;
 		align-items: center;
 		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
 	}
 
 	@media (min-width: 480px) {
