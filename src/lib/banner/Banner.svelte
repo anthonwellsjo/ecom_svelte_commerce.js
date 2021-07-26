e518ee5c5dda8f09ca9a04dd5fa8c4400bb6c453
AnthonWellsjo <anthonwellsjo@gmail.com>
2021-07-26 17:08:48 +0200

    yeah

---
+++ src/lib/banner/Banner.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	export let bcgColor = 'red';
+	export let color = 'black';
+	export let header = 'Header';
+	export let bodyText = 'BodyText';
+</script>
+
+<div style="background-color: {bcgColor}; color: {color}" id="wrapper">
+	<slot />
+	<h3>{header}</h3>
+	<p>{bodyText}</p>
+</div>
+
+<style>
+	#wrapper {
+		width: 100%;
+		position: relative;
+		font-family: 'HM Sans Semi Bold', 'ヒラギノ角ゴ Pro W3', 'Hiragino Kaku Gothic Pro', Osaka,
+			'メイリオ', Meiryo, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+    padding-top: -5px;
+    padding-bottom: 15px;
+	}
+	h3 {
+		font-weight: 600;
+		font-size: 1.5em;
+		margin-bottom: 0;
+	}
+	p {
+		font-size: 0.6em;
+		margin-top: 0;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <script lang="ts">
+	import Banner from '$lib/banner/Banner.svelte';
 	import Counter from '$lib/Counter.svelte';
 </script>
 
@@ -12,17 +13,25 @@
 
 <section>
 	<p id="delivery">Free standard delivery and Click & Collect for Members</p>
+	<Banner
+		bcgColor="#c9002e"
+		color="white"
+		header="Added styles! Up to 50% off"
+		bodyText="Valid on selected items in-store and at hm.com while stocks last. Stock may vary."
+	/>
 </section>
 
 <style>
-  #delivery{
-    font-size: .75em;
-  }
+	#delivery {
+		font-size: 0.7em;
+	}
 	section {
-    padding-top: 30px;
+		padding-top: 30px;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		flex: 1;
 	}
+
+	
 </style>
